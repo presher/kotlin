f036a8030a5f311d21eeacc23ff32ac9797f6443
Victor Jason Presher <presher@users.noreply.github.com>
2022-02-19 13:36:32 -0600

    Add files via upload

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ChoresList.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+enum class ChoresList(val notDone: Boolean){
+    WASH_DISHES(false),
+    SWEEP_FLOORS(false),
+    CLEAN_TOILET(false),
+    CLEAN_TUB(false),
+    VACUUM(false),
+    DRY_DISHES(false),
+    WASH_WINDOWS(false),
+    MAKE_BED(false),
+    CLEAN_ROOM(false),
+    PICKUP_TOYS(false),
+    DUST(false),
+    CLEAN_TABLE(false),
+    MOW_LAWN(false),
+    RAKE_LEAVES(false),
+    CLEAN_BATHROOM_SINK(false),
+    CLEAN_BATHROOM_MIRROR(false),
+    WASH_CLOTHES(false),
+    FOLD_CLOTHES(false),
+
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/ChoresToDo.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+import
+
+class ChoresToDo {
+    val wash = ChoresList.WASH_DISHES.notDone;
+
+    println(wash);
+}
